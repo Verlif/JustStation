76f16522b9594b177657f1e9daba4f2bd510299f
Verlif <1650825929@qq.com>
2021-11-11 18:07:44 +0800

    1. [修改] 数据库新增了测试数据

--- sql/just_station.sql
+++ sql/just_station.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 11/11/2021 09:16:45
+ Date: 11/11/2021 18:06:40
 */
 
 SET NAMES utf8mb4;
@@ -25,7 +25,13 @@ CREATE TABLE `key_`  (
   `key_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'key值ID',
   `key_` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'key值',
   PRIMARY KEY (`key_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of key_
+-- ----------------------------
+INSERT INTO `key_` VALUES (1, 'all');
+INSERT INTO `key_` VALUES (2, 'key');
 
 -- ----------------------------
 -- Table structure for role
@@ -35,7 +41,13 @@ CREATE TABLE `role`  (
   `role_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
   `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
   PRIMARY KEY (`role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 'admin');
+INSERT INTO `role` VALUES (2, 'role');
 
 -- ----------------------------
 -- Table structure for role_key
@@ -48,6 +60,12 @@ CREATE TABLE `role_key`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of role_key
+-- ----------------------------
+INSERT INTO `role_key` VALUES (1, 1);
+INSERT INTO `role_key` VALUES (2, 2);
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -58,7 +76,12 @@ CREATE TABLE `user`  (
   `nickname` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
   PRIMARY KEY (`user_id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'awsl', '$2a$10$CXKIVoC6OthOE5rkl.y2yuQtK3EVStgdw81WXJeE5mBP04JGWw/k.', '阿伟');
 
 -- ----------------------------
 -- Table structure for user_role
@@ -68,5 +91,11 @@ CREATE TABLE `user_role`  (
   `user_id` int(11) NOT NULL COMMENT '用户ID',
   `role_id` int(11) NOT NULL COMMENT '角色ID'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 2);
+INSERT INTO `user_role` VALUES (1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
