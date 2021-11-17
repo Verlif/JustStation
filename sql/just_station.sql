eb0c6af1c24ba1af5bb4b15a7aa1d780696cd397
Verlif <1650825929@qq.com>
2021-11-17 17:12:56 +0800

    1. [修改] 更新了测试数据库结构

--- sql/just_station.sql
+++ sql/just_station.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 11/11/2021 18:06:40
+ Date: 17/11/2021 17:12:04
 */
 
 SET NAMES utf8mb4;
@@ -28,12 +28,6 @@ CREATE TABLE `key_`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of key_
--- ----------------------------
-INSERT INTO `key_` VALUES (1, 'all');
-INSERT INTO `key_` VALUES (2, 'key');
-
--- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
@@ -42,12 +36,6 @@ CREATE TABLE `role`  (
   `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
   PRIMARY KEY (`role_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES (1, 'admin');
-INSERT INTO `role` VALUES (2, 'role');
 
 -- ----------------------------
 -- Table structure for role_key
@@ -60,12 +48,6 @@ CREATE TABLE `role_key`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of role_key
--- ----------------------------
-INSERT INTO `role_key` VALUES (1, 1);
-INSERT INTO `role_key` VALUES (2, 2);
-
--- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -76,12 +58,16 @@ CREATE TABLE `user`  (
   `nickname` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
   PRIMARY KEY (`user_id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user
+-- Table structure for user_favorite
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'awsl', '$2a$10$CXKIVoC6OthOE5rkl.y2yuQtK3EVStgdw81WXJeE5mBP04JGWw/k.', '阿伟');
+DROP TABLE IF EXISTS `user_favorite`;
+CREATE TABLE `user_favorite`  (
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '物品名称'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户喜欢的东西表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_role
@@ -91,11 +77,5 @@ CREATE TABLE `user_role`  (
   `user_id` int(11) NOT NULL COMMENT '用户ID',
   `role_id` int(11) NOT NULL COMMENT '角色ID'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user_role
--- ----------------------------
-INSERT INTO `user_role` VALUES (1, 2);
-INSERT INTO `user_role` VALUES (1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
